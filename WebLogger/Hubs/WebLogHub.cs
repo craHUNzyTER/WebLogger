87dab45a70a8cecb5ff244795d2b031e7b9fe6ff
Serhii Stepanovych <serhii.stepanovych@empeek.com>
2018-05-21 07:57:38 +0300

    Add SignalR package and WebLogHub

---
+++ WebLogger/Hubs/WebLogHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebLogger.Hubs
+{
+    public class WebLogHub : Hub
+    {
+        public Task Subscribe(string groupName)
+        {
+            if (string.IsNullOrEmpty(groupName))
+                throw new ArgumentNullException(string.Format("{0} can not be a null or empty", nameof(groupName)));
+
+            return Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public Task Unsubscribe(string groupName)
+        {
+            if (string.IsNullOrEmpty(groupName))
+                throw new ArgumentNullException(string.Format("{0} can not be a null or empty", nameof(groupName)));
+
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+    }
+}
--- WebLogger/Startup.cs
+++ WebLogger/Startup.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebLogger.Hubs;
 
 namespace WebLogger
 {
@@ -21,6 +18,7 @@ namespace WebLogger
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
             services.AddMvc();
         }
 
@@ -38,6 +36,8 @@ namespace WebLogger
             }
 
             app.UseStaticFiles();
+
+            app.UseSignalR(routes => { routes.MapHub<WebLogHub>("/signalr"); });
 
             app.UseMvc();
         }
