b1420de44f5fef775ad267ca6a8caa5a8165b5e4
Serhii Stepanovych <serhii.stepanovych@empeek.com>
2018-05-21 08:09:39 +0300

    Add web subscriber for log hub

---
+++ WebLogger/wwwroot/js/log.js
@@ -0,0 +1,25 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/signalr")
+    .build();
+
+const logGroupName = "weblog";
+var isSubscribed = false;
+
+var subscribeButton = document.getElementById("subscribeButton");
+
+subscribeButton.addEventListener("click",
+    event => {
+        if (isSubscribed) {
+            connection.invoke("Unsubscribe", logGroupName);
+            isSubscribed = !isSubscribed;
+            subscribeButton.value = "Subscribe";
+        } else {
+            connection.invoke("Subscribe", logGroupName);
+            isSubscribed = !isSubscribed;
+            subscribeButton.value = "Unsubscribe";
+        }
+
+        event.preventDefault();
+    });
+
+connection.start().catch(err => console.error(err.toString()));
